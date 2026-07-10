d97b624b8b71192b9ea3c07cc2c1fbdc974418a7
Griffin Desouza <griffindesouza@gmail.com>
2026-07-10 15:15:55 -0400

    bank.py skeleton code

--- bank_code.py
+++ bank_code.py
@@ -1,8 +1,25 @@
 class BankAcc:
-    def __init__(self, account_number, initial_balance=0):
-        self.account_number = account_number
-        self.balance = initial_balance
+    def __init__(self, name, initial_balance=0):
+        self.name = str(name)
+        self.balance = float(initial_balance)
+        """ Gives name, number, and balance.
+
+        """
 
     def transaction(self, amount):
         self.balance += amount
         return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.transaction
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            return self.balance
+        else:
+            print("Insufficient funds")
+    
+    def get_balance(self):
+        print(self.balance)
