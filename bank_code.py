73e210742e7e5aa33a428c0e96a114cddfcaab6b
Griffin Desouza <griffindesouza@gmail.com>
2026-07-10 14:41:27 -0400

    Add transaction

--- bank_code.py
+++ bank_code.py
@@ -1,4 +1,8 @@
 class BankAcc:
-   def __init__(self, account_number, initial_balance=0):
-       self.account_number = account_number
-       self.balance = initial_balance
+    def __init__(self, account_number, initial_balance=0):
+        self.account_number = account_number
+        self.balance = initial_balance
+
+    def transaction(self, amount):
+        self.balance += amount
+        return self.balance
