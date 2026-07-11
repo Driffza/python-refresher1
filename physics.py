e8cb65490e7fa0c4f2a13f7e2eb2ecd276f5f60f
Griffin Desouza <griffindesouza@gmail.com>
2026-07-11 07:07:59 -0400

    numpy added

--- physics.py
+++ physics.py
@@ -1,16 +1,2 @@
 #physics.py
 import numpy as np
-def calculate_buoyancy(V, density_fluid, g):
-    """
-      Find the buoyant force as F(b) = V * ρ * g
-      V: volume of the displaced fluid
-      density_fluid: density of the fluid = ρ
-      g: gravitational force = 9.81 m/s^2
-      returns:
-        float: product of V * density_fluid * g
-    """
-    g = 9.81
-
-    return V * density_fluid * g
-
- 
